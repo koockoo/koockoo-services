490ac254278ea23941697acb92d56492d2a90ff6
Eugene <job.valchkou@gmail.com>
2014-09-15 23:41:38 -0500

    mapper 2.1.0

--- src/main/java/com/koockoo/chat/controller/AuthController.java
+++ src/main/java/com/koockoo/chat/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.koockoo.chat.controller;
 
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,21 +17,27 @@ import com.koockoo.chat.service.AuthService;
 @RequestMapping(value = "**/auth")
 public class AuthController {
 	
+    private static final Logger log = Logger.getLogger(AuthController.class.getName());
+    
 	@Autowired
 	AuthService authService;
 	
 	/** Operator Signin with credentials */
 	@RequestMapping(value = "signin/operator", method = RequestMethod.POST)
 	public ResponseWrapper<Auth> operatorSignin(@RequestParam String login, @RequestParam String password) {
+	    log.info("operator login attempt by : "+login);
 		Auth auth = null;
 		try {
 			auth = authService.operatorSignin(login, password); 
 		} catch (Exception e) {
+		    log.info("operator login unexpected error : "+login);
 			return new ResponseWrapper<Auth>(ResponseCode.INTERNAL_ERROR, e.getMessage());
 		}
 		if (auth == null) {
+		    log.info("operator login invalid by : "+login);
 			return new ResponseWrapper<Auth>(ResponseCode.INVALID_CREDENTIALS, "Invalid email or password"); 
 		}
+		log.info("operator login success by : "+login);
 		return new ResponseWrapper<Auth>(auth);
 	}
 	
